bf93fea519bbb3f3b9ec91e3d8fd1615504f6989
ellencarlsson <98748113+ellencarlsson@users.noreply.github.com>
2024-01-05 20:04:50 +0100

    Initial Commit

---
+++ Examensarbete_2024 Watch App/Examensarbete_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Examensarbete_2024App.swift
+//  Examensarbete_2024 Watch App
+//
+//  Created by Ellen Carlsson on 2024-01-05.
+//
+
+import SwiftUI
+
+@main
+struct Examensarbete_2024_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
